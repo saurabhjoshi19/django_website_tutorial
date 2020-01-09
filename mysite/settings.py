0408ef087abee0e5b0f011e4b017254c98166ff0
saurabhjoshi <saurabh.joshi190695@gmail.com>
2020-01-09 10:12:42 +0530

    changed db to sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,14 +74,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME' : 'django_db',
+#         'USER': 'saurabh',
+#         'PASSWORD' : 'saurabh',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME' : 'django_db',
-        'USER': 'saurabh',
-        'PASSWORD' : 'saurabh',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
